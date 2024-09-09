26d572aa68ddf98360038444839d3cbbefb6fc27
Jonas Irgens Kylling <jonas@dune.xyz>
2024-09-09 10:03:26 +0200

    Add benchmarks

--- benchmarks/benches/benchmarks.rs
+++ benchmarks/benches/benchmarks.rs
@@ -1,11 +1,11 @@
-use std::sync::Arc;
 use arrow::array::{ArrayRef, StructArray};
 use arrow::datatypes::FieldRef;
 use arrow::record_batch::RecordBatch;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use serde::{Deserialize, Serialize};
 use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
 use serde_arrow::schema::{SchemaLike, TracingOptions};
+use std::sync::Arc;
 
 fn serde_arrow_convert<T: DeserializeOwned>(batch: &RecordBatch) -> Vec<T> {
     serde_arrow::from_record_batch(&batch).unwrap()
@@ -21,24 +21,32 @@ fn setup_record_batch<T: DeserializeOwned + Serialize + From<usize>>(size: usize
     serde_arrow::to_record_batch(&fields, &batch).unwrap()
 }
 
-fn benchmark<T: DeserializeOwned + Serialize + From<usize> + for<'a> arrow_struct::FromArrayRef<'a>>(c: &mut Criterion, size: usize, name: &'static str) {
+fn benchmark<
+    T: DeserializeOwned + Serialize + From<usize> + for<'a> arrow_struct::FromArrayRef<'a>,
+>(
+    c: &mut Criterion,
+    size: usize,
+    name: &'static str,
+) {
     let batch = setup_record_batch::<T>(size);
     let struct_array: StructArray = batch.clone().into();
     let array: ArrayRef = Arc::new(struct_array);
-    c.bench_function(&format!("serde_arrow {}", name), |b| b.iter_with_large_drop(|| serde_arrow_convert::<T>(black_box(&batch))));
-    c.bench_function(&format!("arrow_struct {}", name), |b| b.iter_with_large_drop(|| arrow_struct_convert::<T>(black_box(&array))));
+    c.bench_function(&format!("serde_arrow {}", name), |b| {
+        b.iter_with_large_drop(|| serde_arrow_convert::<T>(black_box(&batch)))
+    });
+    c.bench_function(&format!("arrow_struct {}", name), |b| {
+        b.iter_with_large_drop(|| arrow_struct_convert::<T>(black_box(&array)))
+    });
 }
 
 #[derive(Deserialize, Serialize, arrow_struct::Deserialize)]
 struct Small {
-    i64: i64
+    i64: i64,
 }
 
 impl From<usize> for Small {
     fn from(value: usize) -> Self {
-        Self {
-            i64: value as i64
-        }
+        Self { i64: value as i64 }
     }
 }
 
